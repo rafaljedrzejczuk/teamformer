b7c4731bd9b97979f56cf218ec0efd7553c195fd
unknown <rafal.jedrzejczuk1@DLOLENB041.driveline.gkn.com>
2015-01-27 15:44:44 +0100

    MEGA UPDATE (some files forgotten)
    + persistance.xml for UNIT TESTS
    \
      - import.sql with statements loaded after EntityManager is created
      - added logs for more information (look for log4j.properties in test/resources)
    + DaoLogged
    \
      - added registerAccount(Strings) for tests (in future register and logged will be merged)
      - added removeAccount(Account) for tests
    + code cleaned (not used lines deleted)
    + sequence generators added to all entties (in import.sql all SQLs to create them for TESTS)
    + from this UPDATE DAOs do NOT invokes other methods than LOADING and SAVING, for rest BEANS are responsible for
    + all XHTMLs updated with methods (look above)

---
+++ src/test/resources/import.sql
@@ -0,0 +1,5 @@
+create sequence ACCOUNT_ID start with 1 increment by 1;
+create sequence TOPIC_ID start with 1 increment by 1;
+create sequence POST_ID start with 1 increment by 1;
+create sequence TEAM_ID start with 1 increment by 1;
+create sequence USERGROUP_ID start with 1 increment by 1;
